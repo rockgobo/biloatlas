2a5cb7d1e36577540e7a1c23afe683aa36effe0a
Christian Matyas <christian.matyas@lifbi.de>
2016-02-25 11:43:50 +0100

    redesign

--- app/components/topics-buttons/topics-buttons.component.js
+++ app/components/topics-buttons/topics-buttons.component.js
@@ -8,10 +8,20 @@
         component('topicsButtons',{
         templateUrl: 'app/components/topics-buttons/topics-buttons.component.html',
         controller: function(TopicData,$routeParams){
-            this.topics = [];
+            this.chunkedTopics = [];
+            
             TopicData.getTopics().then(function(topics){
-                this.topics = topics;
+                topics = topics.sort(function(a,b){return a.layers.length < b.layers.length});
+                this.chunkedTopics = chunk(topics, 3);
             }.bind(this));
+            
+            function chunk(arr, size) {
+                var newArr = [];
+                for (var i=0; i<arr.length; i+=size) {
+                    newArr.push(arr.slice(i, i+size));
+                }
+                return newArr;
+            }
         }
     });
 })();
