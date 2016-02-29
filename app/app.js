71f80795a0e546eab7727630747fa2497a72b787
Christian Matyas <christian.matyas@lifbi.de>
2016-02-29 16:15:44 +0100

    + removed old cordava code

--- app/app.js
+++ app/app.js
@@ -6,10 +6,4 @@
     */
     angular.module('biloAtlas', ['ngRoute','nvd3']);
     
-    //config for cordova app
-    /*
-        .config(['$compileProvider', function ($compileProvider) {
-            //Add ms-appx-web prefix to whitelist otherwise windows 10 add "unsafe:" prefix to links and breaks angularjs links
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|ms-appx-web|x-wmapp0):/);
-    }]);*/
 })();
