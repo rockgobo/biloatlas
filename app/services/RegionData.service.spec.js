f71a227b44bad87cabc7b3e2b6ff00b591be7c45
Christian Matyas <christian.matyas@lifbi.de>
2016-04-25 15:29:47 +0200

    standard format conform

--- app/services/RegionData.service.spec.js
+++ app/services/RegionData.service.spec.js
@@ -2,57 +2,58 @@
  * Created by CMatyas on 03.02.2016.
  * Jasmine test for RegionData service
  */
+/*globals describe:true beforeEach:true it:true expect:true inject:true*/
 
-describe('A RegionData service', function(){
-    "use strict"
+describe('A RegionData service', function () {
+  'use strict'
 
-    var RegionData;
-    beforeEach(module('biloAtlas'));
-    beforeEach(inject(function (_RegionData_) {
-        RegionData = _RegionData_;
-    }))
-    
-    //Public API Tests
-    describe('Public API', function () {
-        it('should provide a getRegions Api method', function () { 
-            expect(RegionData.getRegions).toBeDefined();
-            expect(typeof RegionData.getRegions).toBe('function'); 
-        });
-        //Get Tests
-        it('should provide a getRegionById Api method', function () {
-            expect(RegionData.getRegionById).toBeDefined();
-            expect(typeof RegionData.getRegionById).toBe('function');
-        }) 
+  var RegionData
+  beforeEach(module('biloAtlas'))
+  beforeEach(inject(function (_RegionData_) {
+    RegionData = _RegionData_
+  }))
+
+  // Public API Tests
+  describe('Public API', function () {
+    it('should provide a getRegions Api method', function () {
+      expect(RegionData.getRegions).toBeDefined()
+      expect(typeof RegionData.getRegions).toBe('function')
+    })
+    // Get Tests
+    it('should provide a getRegionById Api method', function () {
+      expect(RegionData.getRegionById).toBeDefined()
+      expect(typeof RegionData.getRegionById).toBe('function')
+    })
+  })
+
+  describe('getRegions method', function () {
+    it('should provide a getRegions Api method that return a list of regions', function () {
+      RegionData.getRegions().then(function (regions) {
+        expect(regions.length).toBeGreaterThan(0)
+        expect(regions instanceof Array).toBe(true)
+      })
+    })
+  })
+
+  describe('getRegionById method', function () {
+    it('should return the topic with a given id', function () {
+      // Extract a topic as test case
+      RegionData.getRegions().then(function (regions) {
+        expect(regions.length).toBeGreaterThan(0)
+        // Test if the object return by id is equal to the extracted topic from the list
+        var regionToGet = regions[0]
+        RegionData.getRegionById(regionToGet.id).then(function (region) {
+          expect(region instanceof Object).toBe(true)
+          expect(region.id).toBe(regionToGet.id)
+          expect(region).toEqual(regionToGet)
+        })
+      })
     })
 
-    describe('getRegions method', function () {
-        it('should provide a getRegions Api method that return a list of regions', function () {
-            RegionData.getRegions().then(function (regions){
-                expect(regions.length).toBeGreaterThan(0);
-                expect(regions instanceof Array).toBe(true);
-            })
-        })
+    it('should return null for a unknown id', function () {
+      RegionData.getTopicById(9999999999).then(function (region) {
+        expect(region).toBeNull()
+      })
     })
-    
-    describe('getRegionById method', function () {
-        it('should return the topic with a given id', function () {
-            //Extract a topic as test case
-            RegionData.getRegions().then(function(regions){
-                expect(regions.length).toBeGreaterThan(0);
-                //Test if the object return by id is equal to the extracted topic from the list
-                var regionToGet = regions[0];
-                RegionData.getRegionById(regionToGet.id).then(function(region){
-                    expect(region instanceof Object).toBe(true);
-                    expect(region.id).toBe(regionToGet.id);
-                    expect(region).toEqual(regionToGet);
-                })
-            })
-        })
-
-        it('should return null for a unknown id', function () {
-            RegionData.getTopicById(9999999999).then(function(region){
-                expect(region).toBeNull();
-            })
-        })
-    })
+  })
 })
--- app/services/TopicData.service.spec.js
+++ app/services/TopicData.service.spec.js
@@ -2,61 +2,61 @@
  * Created by CMatyas on 03.12.2015.
  * Jasmine test for TopicData service. More on Jamsine: http://jasmine.github.io/
  */
+/*globals describe:true beforeEach:true it:true expect:true inject:true*/
+
 describe('A TopicData service', function () {
-    "use strict"
+  'use strict'
 
-    var TopicData;
-    beforeEach(module('biloAtlas'));
-    beforeEach(inject(function (_TopicData_) {
-        TopicData = _TopicData_;
-    }));
+  var TopicData
+  beforeEach(module('biloAtlas'))
+  beforeEach(inject(function (_TopicData_) {
+    TopicData = _TopicData_
+  }))
 
-    //Public API Tests
-    describe('Public API', function () {
-        it('should provide a getTopics Api method', function () { 
-            expect(TopicData.getTopics).toBeDefined();
-            expect(typeof TopicData.getTopics).toBe('function'); 
-        });
-        //Get Tests
-        it('should provide a getopicById Api method', function () {
-            expect(TopicData.getTopicById).toBeDefined();
-            expect(typeof TopicData.getTopicById).toBe('function');
-        }) 
-    });
-
-    describe('getTopics method', function () {
-        it('should provide a getTopics Api method that return a list of topics', function () {
-            TopicData.getTopics().then(function (topics){
-                console.log(topics);
-
-                expect(topics.length).toBeGreaterThan(0);
-                expect(topics instanceof Array).toBe(true);
-                expect(Array.isArray(topics)).toBe(true);
-
-            });
-        })
-    });
-
-
-    describe('getTopicById method', function () {
-        it('should return the topic with a given id', function () {
-            //Extract a topic as test case
-            TopicData.getTopics().then(function(topics){
-                expect(topics.length).toBeGreaterThan(0);
-                //Test if the object return by id is equal to the extracted topic from the list
-                var topicToGet = topics[0];
-                TopicData.getTopicById(topicToGet.id).then(function(topic){
-                    expect(topic instanceof Object).toBe(true);
-                    expect(topic.id).toBe(topicToGet.id);
-                    expect(topic).toEqual(topicToGet);
-                });
-            });
-        });
-
-        it('should return null for a unknown id', function () {
-            TopicData.getTopicById(9999999999).then(function(topic){
-                expect(topic).toBeNull();
-            })
-        })
+  // Public API Tests
+  describe('Public API', function () {
+    it('should provide a getTopics Api method', function () {
+      expect(TopicData.getTopics).toBeDefined()
+      expect(typeof TopicData.getTopics).toBe('function')
     })
-});
+    // Get Tests
+    it('should provide a getopicById Api method', function () {
+      expect(TopicData.getTopicById).toBeDefined()
+      expect(typeof TopicData.getTopicById).toBe('function')
+    })
+  })
+
+  describe('getTopics method', function () {
+    it('should provide a getTopics Api method that return a list of topics', function () {
+      TopicData.getTopics().then(function (topics) {
+        console.log(topics)
+
+        expect(topics.length).toBeGreaterThan(0)
+        expect(topics instanceof Array).toBe(true)
+        expect(Array.isArray(topics)).toBe(true)
+      })
+    })
+  })
+
+  describe('getTopicById method', function () {
+    it('should return the topic with a given id', function () {
+      // Extract a topic as test case
+      TopicData.getTopics().then(function (topics) {
+        expect(topics.length).toBeGreaterThan(0)
+        // Test if the object return by id is equal to the extracted topic from the list
+        var topicToGet = topics[0]
+        TopicData.getTopicById(topicToGet.id).then(function (topic) {
+          expect(topic instanceof Object).toBe(true)
+          expect(topic.id).toBe(topicToGet.id)
+          expect(topic).toEqual(topicToGet)
+        })
+      })
+    })
+
+    it('should return null for a unknown id', function () {
+      TopicData.getTopicById(9999999999).then(function (topic) {
+        expect(topic).toBeNull()
+      })
+    })
+  })
+})
