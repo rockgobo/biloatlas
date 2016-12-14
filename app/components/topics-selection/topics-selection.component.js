a36b95e33613f8a688b0e0e957a5e9524f70f5c3
Christian Matyas <christian.matyas@lifbi.de>
2016-12-14 16:46:51 +0100

    #60 take the topic specified from the route if possible

--- app/components/topics-selection/topics-selection.component.js
+++ app/components/topics-selection/topics-selection.component.js
@@ -12,14 +12,20 @@
     bindings: {
       topic: '='
     },
-    controller: function (TopicData, RegionData) {
+    controller: function (TopicData, RegionData, $routeParams) {
       this.topics = []
 
       // Loading all topics
       TopicData.getTopics().then(function (topics) {
         this.topics = topics.filter(function (t) { return (t.layers.length > 0 || t.layerGroups.length > 0) })
-        if (this.topics.length > 0) {
-          this.topic = this.topics[0]
+        if (this.topics.length > 0 ) {
+          //If a topic is set as route parameter try to lookup the topic with the same id
+          var topic_id = $routeParams.topicid ? $routeParams.topicid : false
+          var topic = this.topics.find(function(t){ return topic_id == t.id })
+          if(topic === undefined){
+            this.topic = this.topics[0]
+          }
+          this.topic = topic
         }
       }.bind(this))
     }
