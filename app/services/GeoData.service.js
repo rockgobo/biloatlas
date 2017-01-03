72a6edce137401e48f6996f12ba3760e6ab1d701
Christian Matyas <christian.matyas@gmail.com>
2017-01-03 15:55:28 +0100

    #77 add shortname to default data template

--- app/services/GeoData.service.js
+++ app/services/GeoData.service.js
@@ -109,9 +109,9 @@
         })
         .map(function (f) {
           if (f.properties.ID_3 === id) {
-            return {id: id, name: f.properties.NAME_3, value: value, year: year}
+            return {id: id, name: f.properties.NAME_3, shortName: f.properties.NAME_3, value: value, year: year}
           }
-          return {id: f.properties.ID_3, name: f.properties.NAME_3, value: defaultValue, year: year}
+          return {id: f.properties.ID_3, name: f.properties.NAME_3,shortName: f.properties.NAME_3, value: defaultValue, year: year}
         })
 
         if (regions.length > 0) {
