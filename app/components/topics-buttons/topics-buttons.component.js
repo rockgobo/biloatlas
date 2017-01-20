35f8181a447c3459a8b59452a5be349c9458a7ea
Christian Matyas <christian.matyas@lifbi.de>
2017-01-20 16:05:43 +0100

    #89 two column start page

--- app/components/topics-buttons/topics-buttons.component.js
+++ app/components/topics-buttons/topics-buttons.component.js
@@ -9,25 +9,13 @@
   angular.module('biloAtlas').component('topicsButtons', {
     templateUrl: 'app/components/topics-buttons/topics-buttons.component.html',
     controller: function (TopicData, GeoData, $routeParams) {
-      this.chunkedTopics = []
+      this.topics = []
       this.loading = true
       TopicData.getTopics().then(function (topics) {
-        topics = topics
-                    //.filter(function (t) { return t.layers.length > 0 || t.layerGroups.length > 0 })
-                    //.sort(function (a, b) { return a.layers.length < b.layers.length })
-        this.chunkedTopics = chunk(topics, 3)
-        this.loading = false
+        this.topics = topics
       }.bind(this))
 
       this.mapData = GeoData.getDataByValue(0,1)
-
-      function chunk (arr, size) {
-        var newArr = []
-        for (var i = 0; i < arr.length; i += size) {
-          newArr.push(arr.slice(i, i + size))
-        }
-        return newArr
-      }
     }
   })
 })()
