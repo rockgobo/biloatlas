83b5f64f9342ac3dff6958b01694051942c2398b
Christian Matyas <christian.matyas@lifbi.de>
2017-02-06 16:23:36 +0100

    #118 Adds white for scales with minimum value of 0

--- app/components/colorbrewer-picker/colorbrewer-picker.component.js
+++ app/components/colorbrewer-picker/colorbrewer-picker.component.js
@@ -18,13 +18,45 @@
       //this.colors = ColorBrewer.colors
       this.colors = {
         color1: {
-          9: Colors.getPrimary()
+          9: Colors.getPrimary(),
+          5: Colors.getPrimary5(),
+          3: Colors.getPrimary3()
         },
         color2: {
-          9: Colors.getSecondary()
+          9: Colors.getSecondary(),
+          5: Colors.getSecondary5(),
+          3: Colors.getSecondary3()
         }
       }
       this.opencolors = false
+
+      this.currentMin = 0
+      this.currentSchema = []
+
+      /**
+       * Adds white color to the schema if the current min value is 0
+       * if the min value is not 0 the white color is removed
+       */
+      this.pickSchema = function(schema){
+        if(schema === undefined) return
+
+        this.schema = schema
+        this.currentSchema = Object.assign([], schema) //clone data in new array
+        
+        if(this.currentSchema.indexOf('#FFFFFF') === -1 && this.currentMin+'' === '0'){ 
+          this.currentSchema.unshift('#FFFFFF')
+        }
+        else if(this.currentSchema.indexOf('#FFFFFF') === 0 && this.currentMin+'' !== '0'){
+          this.currentSchema.shift()
+        }
+      }
+
+      this.$onChanges = function(changesObj){
+        if(changesObj.min){
+          this.currentMin = changesObj.min.currentValue
+          this.pickSchema(this.schema)
+        }
+      }
     }
   })
 })()
---
+++ app/components/colorbrewer-picker/colorbrewer-picker.component.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Spec for the color picker conponent
+ * author: Christian Matyas
+ */
+
+describe('A color picker component', function(){
+    it('has a minimum value',function(){ })
+    it('has a maximum value',function(){ })
+    it('has a different colorschemas to choice from',function(){ })
+    it('adds a white color to the choosen schema if the minimum value is equal zero (github #118)',function(){ })
+})
