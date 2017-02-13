523abfe2fb58151962f43441e0dac34902696d19
Christian Matyas <christian.matyas@lifbi.de>
2017-02-13 16:31:57 +0100

    spec for region view

---
+++ app/components/region-view/region-view.component.spec.js
@@ -0,0 +1,18 @@
+describe('A region view', function(){
+    describe('shows a map of upper frankonia', function(){
+        it('highlights the current region on the map', function(){})
+        it('highlight color is purple',function(){})
+        it('other regions are colored in green',function(){})
+        it('brings up a tooltip on mouseover showing the regions name',function(){})
+        it('navigate to the region on a click',function(){})
+    })
+    describe('shows the current region on a map', function(){
+        it('that contains any educational institute in that region', function(){})
+        it('schools are shown in circles',function(){})
+    })
+    describe('shows a list of all topics', function(){
+        it('a line diagram is used for continuous data', function(){})
+        it('a dot chart is used for non continous data',function(){})
+        it('a bar chart is used for data with only two years',function(){})
+    })
+})
